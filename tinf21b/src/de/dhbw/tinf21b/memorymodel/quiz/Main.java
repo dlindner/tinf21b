0044fd62b7d14bb0ca21ed23a1dce83e32e77be5
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2023-04-28 08:27:17 +0200

    Vorlesung 3: Memory Model Quiz in Code

---
+++ tinf21b/src/de/dhbw/tinf21b/memorymodel/quiz/Main.java
@@ -0,0 +1,12 @@
+package de.dhbw.tinf21b.memorymodel.quiz;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println(Scenario.runFor((int) 1E5, Scenario1::new));
+		//System.out.println(Scenario.runFor((int) 1E5, Scenario2::new));
+		//System.out.println(Scenario.runFor((int) 1E5, Scenario3::new));
+		//System.out.println(Scenario.runFor((int) 1E5, Scenario4::new));
+		//System.out.println(Scenario.runFor((int) 1E5, Scenario5::new));
+	}
+}
